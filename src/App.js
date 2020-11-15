98052765ea5daecff7b443f36a03402471c56982
Skynet966 <satishkumaryadav72@gmail.com>
2020-11-15 11:48:38 +0530

    Google Sign in and email and password auth done

--- src/App.js
+++ src/App.js
@@ -6,12 +6,10 @@ import HomePage from "./pages/homepage/homepage.component";
 import Shop from "./pages/shop/shop.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sing-up/sign-in-and-sing-up.component";
 import Header from "./components/header/header.component";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
-class App extends React.Component
-{
-  constructor ()
-  {
+class App extends React.Component {
+  constructor() {
     super();
 
     this.state = {
@@ -21,24 +19,31 @@ class App extends React.Component
 
   unsubscribeFromAuth = null;
 
-  componentDidMount ()
-  {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user =>
-    {
-      this.setState({ currentUser: user });
-      console.log(user);
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot((snapShot) => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            },
+          });
+          console.log(this.state);
+        });
+      }
+      this.setState({ currentUser: userAuth });
     });
   }
-  componentWillUnmount ()
-  {
+  componentWillUnmount() {
     this.unsubscribeFromAuth();
   }
 
-  render ()
-  {
+  render() {
     return (
       <div className="App">
-        <Header currentUser={this.state.currentUser}/>
+        <Header currentUser={this.state.currentUser} />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route exact path="/shop" component={Shop} />
--- src/pages/sign-in-and-sing-up/sign-in-and-sing-up.component.jsx
+++ src/pages/sign-in-and-sing-up/sign-in-and-sing-up.component.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import SingIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
 
 import './sign-in-and-sing-up.styles.sass';
 
 const SignInAndSignUpPage = () =>(
-    <div className='sign-in-and-sing-up'>
+    <div className='sign-in-and-sign-up'>
         <SingIn/>
+        <SignUp/>
     </div>
 )
 export default SignInAndSignUpPage;
