dadaae4b2972f2bbaa8fd751157f73892785dbb9
Skynet966 <satishkumaryadav72@gmail.com>
2020-11-17 15:10:57 +0530

    redux-persistor configured but not working

--- src/index.js
+++ src/index.js
@@ -2,18 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import { store, persistor } from './redux/store';
 
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import store from './redux/store';
+import {} from '';
 
 ReactDOM.render(
 	<React.StrictMode>
 		<Provider store={store}>
 			<BrowserRouter>
-				<App />
+				<PersistGate persistor={persisotr}>
+					<App />
+				</PersistGate>
 			</BrowserRouter>
 		</Provider>
 	</React.StrictMode>,
